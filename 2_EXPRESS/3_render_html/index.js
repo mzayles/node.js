2007e41147ae94ec40d357c0fbe997a5577f97fc
Mariana Alves <102690791+mzayles@users.noreply.github.com>
2023-09-28 13:20:51 -0300

    Render HTML finalizado.

--- 2_EXPRESS/3_render_html/index.js
+++ 2_EXPRESS/3_render_html/index.js
@@ -1,11 +1,16 @@
-const express = require("express")
+const express = require("express");
+const path  = require("path");
 
-const app = express()
+const app = express();
+
+const caminhoBase  = path.join(__dirname, 'templates');
 
 app.get('/', (requisicao, resposta) => {
-    resposta.send("Estou funcionando!")
+    resposta.sendFile(`${caminhoBase}/index.html`)
 })
 
-app.listen(3000, () => {
-    console.log("Servidor rodando na porta 3000")
-})
+app.listen(3000, () =>{
+    console.log("servidor rodando na porta 3000")
+});
+
+// http://localhost:3000/
